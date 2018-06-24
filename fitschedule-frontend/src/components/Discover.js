9cc3c91da37210fe090544191fbaf7dd32ca6357
cerireyhan <ceri.reyhan@teleclinic.com>
2018-06-24 20:32:02 +0200

    Add distance field, fix services

--- fitschedule-frontend/src/components/Discover.js
+++ fitschedule-frontend/src/components/Discover.js
@@ -5,17 +5,91 @@ import {withRouter} from 'react-router-dom'
 import Page from "./Page";
 import Map from "./Map";
 import SearchBar from "./SearchBar";
+import {Snackbar} from "react-md";
 
 class Discover extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {geolocation: null};
         this.searchSubmit = this.searchSubmit.bind(this);
+        this.dismissToast = this.dismissToast.bind(this);
         this.locationSubmit = this.locationSubmit.bind(this);
         this.useGeolocation = this.useGeolocation.bind(this);
+        this.showPosition = this.showPosition.bind(this);
+        this.showError = this.showError.bind(this);
     }
 
+    componentDidMount() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.showPosition, this.showError);
+        } else {
+            console.log('[MapComponent] Browser does not support geolocation');
+            this.addToast("Browser doesn't support location.");
+        }
+    }
+
+    showPosition(position) {
+        console.log('[MapComponent] Geolocation: lat:', position.coords.latitude, 'long:', position.coords.longitude);
+        const geolocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        this.setState({
+            geolocation: geolocation
+        });
+    }
+
+    showError(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                console.log('[MapComponent] User denied the request for Geolocation');
+                this.addToast("Please enable location permission.");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                console.log('[MapComponent] Location information is unavailable');
+                this.addToast("Location information is unavailable. Please enter manually.");
+                break;
+            case error.TIMEOUT:
+                console.log('[MapComponent] The request to get user location timed out');
+                this.addToast("Please enable location from browser settings.");
+                break;
+            case error.UNKNOWN_ERROR:
+                console.log('[MapComponent] An unknown error occurred');
+                this.addToast("Unknown error. Location information is unavailable.");
+                break;
+        }
+    }
+
+    addToast(text, action) {
+        this.setState((state) => {
+            const toasts = state.toasts.slice();
+            toasts.push({text, action});
+            return {toasts};
+        });
+    }
+
+    dismissToast() {
+        const [, ...toasts] = this.state.toasts;
+        this.setState({toasts});
+    };
+
     searchSubmit(value) {
-        console.log('[DiscoverComponent] Search submit', value);
+        const query = {
+            course: value.course,
+            dist: value.dist
+        };
+        if (value.useGeolocation && this.state.geolocation) {
+            query.coord = this.state.geolocation;
+        } else if (value.coord) {
+            query.coord = value.coord;
+        }
+
+        if (query.coord) {
+            console.log('[DiscoverComponent] Submitting query', query);
+            this.props.onSubmit(query);
+        } else {
+            this.addToast("Please enable location or enter it manually.");
+        }
 
     }
 
@@ -24,16 +98,20 @@ class Discover extends React.Component {
     }
 
     useGeolocation() {
-
+        this.child.focusGeolocation();
     }
 
     render() {
         return (
             <Page>
-                <SearchBar useGeolocation={() => this.useGeolocation()} onSubmit={(value) => this.searchSubmit(value)}/>
-                <Map onSubmit={(value) => this.locationSubmit(value)}/>
+                <SearchBar useGeolocation={() => this.useGeolocation()}
+                           onSubmit={(value) => this.searchSubmit(value)}/>
+                <Map geolocation={this.state.geolocation}
+                     onSubmit={(value) => this.locationSubmit(value)}
+                     onRef={ref => (this.child = ref)}/>
+                <Snackbar toasts={this.state.toasts} autohide={true} onDismiss={this.dismissToast}/>
             </Page>
-        )
+        );
     }
 }
 
--- fitschedule-frontend/src/components/SearchBar.js
+++ fitschedule-frontend/src/components/SearchBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import {Button, FontIcon, TextField} from "react-md";
+import {Button, FontIcon, SelectField, TextField} from "react-md";
 import {withRouter} from "react-router-dom";
 import LocationSearchField from "./LocationSearchField";
 
 const searchBarStyle = {display: 'flex', justifyContent: 'space-around', alignItems: 'center'};
 const textFieldStyle = {width: '20rem'};
 const buttonStyle = {verticalAlign: 'bottom'};
-const pinStyle = {fontSize: 30};
+const pinStyle = {fontSize: 30, cursor: 'pointer'};
+const NUMBER_ITEMS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 class SearchBar extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {course: '', useGeolocation: true};
-
         this.handleChangeCourse = this.handleChangeCourse.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.locationSubmit = this.locationSubmit.bind(this);
         this.useGeolocation = this.useGeolocation.bind(this);
+        this.handleChangeDistance = this.handleChangeDistance.bind(this);
     }
 
     handleChangeCourse(value) {
@@ -30,20 +31,25 @@ class SearchBar extends React.Component {
         let search = {
             course: this.state.course,
             coord: this.state.coord,
-            useGeolocation: this.state.useGeolocation
+            useGeolocation: this.state.useGeolocation,
+            dist: this.state.dist
         };
 
         this.props.onSubmit(search);
     }
 
     locationSubmit(value) {
-        this.setState({coord: value});
+        this.setState({coord: value, useGeolocation: false});
     }
 
     useGeolocation() {
-        this.setState({useGeolocation: !this.state.useGeolocation});
+        this.setState({useGeolocation: true});
         this.props.useGeolocation();
     }
+
+    handleChangeDistance(value, index, event, details) {
+        this.setState({dist: value});
+    };
 
     render() {
         return (
@@ -57,6 +63,14 @@ class SearchBar extends React.Component {
                     onChange={this.handleChangeCourse}/>
                 <LocationSearchField onSubmit={(value) => this.locationSubmit(value)}/>
                 <FontIcon primary style={pinStyle} onClick={this.useGeolocation}>my_location</FontIcon>
+                <SelectField
+                    id="distanceField"
+                    label="Distance in KM"
+                    placeholder="Placeholder"
+                    onChange={this.handleChangeDistance}
+                    className="md-cell"
+                    menuItems={NUMBER_ITEMS}
+                />
                 <Button id="submit" type="submit" style={buttonStyle} raised secondary>Discover</Button>
             </form>
         );
