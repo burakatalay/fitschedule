50f61d24f9563c162c0c697b69006733418a7cc3
burakatalay <atalay.burak3@gmail.com>
2018-06-26 22:57:57 +0200

    Committed out a line that caused backend to crash

--- fitschedule-backend/src/routes/schedule.js
+++ fitschedule-backend/src/routes/schedule.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const middlewares    = require('../middlewares');
 const scheduleController = require('../controllers/schedule');
 
-router.post('/', middlewares.checkAuthentication, scheduleController.addCourse); // Add course to schedule
+//router.post('/', middlewares.checkAuthentication, scheduleController.addCourse); // Add course to schedule
 router.get('/', middlewares.checkAuthentication, scheduleController.list); // List my courses in my schedule
 
 module.exports = router;
