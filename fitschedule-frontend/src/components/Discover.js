141cf0bf8a46e6c35f43d737ad303a422746fdba
cerireyhan <ceri.reyhan@teleclinic.com>
2018-06-27 21:48:16 +0200

    Add infowindow

--- fitschedule-frontend/src/components/Discover.js
+++ fitschedule-frontend/src/components/Discover.js
@@ -37,29 +37,66 @@ class Discover extends React.Component {
         DiscoverService.getCourses(query.course, query.coord.lat, query.coord.lng, query.dist)
             .then((courses) => {
                 console.log('[DiscoverComponent] Success getting course providers', courses);
-                this.clearMarkers();
+                this.clearCourseMarkers();
                 courses.forEach((course) => {
                     const geolocation = {
                         'lng': course.location.coordinates[0],
                         'lat': course.location.coordinates[1]
                     };
-                    this.createMarker(geolocation);
+                    this.createCourseMarker(geolocation);
                 });
             }).catch((e) => {
             console.error('[DiscoverComponent] Error getting course providers', e);
         });
     }
 
-    createMarker(geolocation) {
+    createCourseMarker(geolocation) {
+        const contentString = '<div id="content">' +
+            '<div id="siteNotice">' +
+            '</div>' +
+            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+            '<div id="bodyContent">' +
+            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            'sandstone rock formation in the southern part of the ' +
+            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+            'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+            'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+            'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+            'Aboriginal people of the area. It has many springs, waterholes, ' +
+            'rock caves and ancient paintings. Uluru is listed as a World ' +
+            'Heritage Site.</p>' +
+            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+            'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+            '(last visited June 22, 2009).</p>' +
+            '</div>' +
+            '</div>';
+        const infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+        const marker = new google.maps.Marker({
+            position: geolocation,
+            map: this.map,
+            infowindow: infowindow
+        });
+        marker.addListener('click', () => {
+            this.closeAllInfoWindows();
+            infowindow.open(this.map, marker);
+        });
         const markers = this.state.markers;
-        markers.push(new google.maps.Marker({
-                position: geolocation,
-                map: this.map
-            })
-        );
+        markers.push(marker);
+        this.setState({
+            markers: markers
+        });
     }
 
-    clearMarkers() {
+    closeAllInfoWindows() {
+        this.state.markers.forEach((marker) => {
+            marker.infowindow.close(this.map, marker);
+        });
+    }
+
+    clearCourseMarkers() {
         const markers = this.state.markers;
         markers.forEach(marker => {
             marker.setMap(null);
