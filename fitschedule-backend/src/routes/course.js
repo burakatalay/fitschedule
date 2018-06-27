7f0f9ab464b5eb854cad2098d5de4ea5da944dfa
burakatalay <atalay.burak3@gmail.com>
2018-06-27 21:02:49 +0200

    Changed routes

--- fitschedule-backend/src/routes/course.js
+++ fitschedule-backend/src/routes/course.js
@@ -8,10 +8,10 @@ const CourseController = require('../controllers/course');
 
 
 router.get('/', CourseController.list); // List all courses
-router.get('/:courseID', middlewares.checkAuthentication, CourseController.getCourseDetails); // Get details of course from course ID
+router.get('/getCourseDetails/:courseID', middlewares.checkAuthentication, CourseController.getCourseDetails); // Get details of course from course ID
 router.post('/', /*middlewares.checkAuthentication,*/ middlewares.checkInstructor, CourseController.createCourseAsInstructor); // Create a new course
 router.post('/createcourseasfitnesscenter', CourseController.createCourseAsFitnessCenter); // Create a new course
-router.get('/', middlewares.checkAuthentication, CourseController.findCoursesByNameAndLocation); // List all courses
+router.get('/getcoursesbyname/', middlewares.checkAuthentication, CourseController.findCoursesByNameAndLocation); // List all courses
 //router.get('/:id', CourseController.read); // Read all courses by Name
 //router.put('/:id', middlewares.checkAuthentication, CourseController.update); // Update a course by Id
 //router.delete('/:id', middlewares.checkAuthentication, CourseController.remove); // Delete a course by Id
