485c658c8d44d8504eabff0a80e0ec88bd188c97
cerireyhan <ceri.reyhan@teleclinic.com>
2018-06-27 22:02:59 +0200

    fix kebab

--- fitschedule-frontend/src/components/KebabMenu.js
+++ fitschedule-frontend/src/components/KebabMenu.js
@@ -41,10 +41,10 @@ class KebabMenu extends React.Component {
                 icon
                 className={this.props.className}
                 menuItems={this.state.user ? [
-                    <ListItem key={1} leftAvatar={<Avatar icon={<FontIcon>account_circle</FontIcon>}/>} primaryText={this.state.user.firstname}/>,
+                    <ListItem key={1} leftAvatar={<Avatar icon={<FontIcon>account_circle</FontIcon>}/>} primaryText={this.state.user.email}/>,
                     <ListItem key={2} leftAvatar={<Avatar icon={<FontIcon>add</FontIcon>}/>} primaryText="Add Course" onClick={() => this.props.history.push('/add')}/>,
-                    <ListItem key={2} leftAvatar={<Avatar>MS</Avatar>} primaryText="My Schedule" onClick={() => this.props.history.push('/schedule')}/>,
-                    <ListItem key={3} primaryText="Logout" onClick={() => this.logout()}/>
+                    <ListItem key={3} leftAvatar={<Avatar>MS</Avatar>} primaryText="My Schedule" onClick={() => this.props.history.push('/schedule')}/>,
+                    <ListItem key={4} primaryText="Logout" onClick={() => this.logout()}/>
                 ]: [<ListItem key={1} primaryText="Login" onClick={() => this.props.history.push('/login')}/>]}
             >
                 more_vert
