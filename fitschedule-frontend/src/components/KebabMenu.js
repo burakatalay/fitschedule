11bde022a1453ef411df156214d732ce7599102e
cerireyhan <ceri.reyhan@teleclinic.com>
2018-07-01 16:59:55 +0200

    Show/Hide AddCourse option in KebabMenu according to user type

--- fitschedule-frontend/src/components/KebabMenu.js
+++ fitschedule-frontend/src/components/KebabMenu.js
@@ -2,17 +2,17 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { MenuButton, ListItem, Avatar, FontIcon } from 'react-md';
-import { withRouter } from 'react-router-dom'
+import {Avatar, FontIcon, ListItem, MenuButton} from 'react-md';
+import {withRouter} from 'react-router-dom'
 
-import UserService from  '../services/UserService';
+import UserService from '../services/UserService';
 
 
 class KebabMenu extends React.Component {
 
     constructor(props) {
         super(props);
-        
+
         this.state = {
             user: UserService.isAuthenticated() ? UserService.getCurrentUser() : undefined
         }
@@ -33,18 +33,25 @@ class KebabMenu extends React.Component {
     }
 
     render() {
+        const menuItems = [];
+        if(this.state.user) {
+            menuItems.push(<ListItem key={1} leftAvatar={<Avatar icon={<FontIcon>account_circle</FontIcon>}/>} primaryText={this.state.user.email}/>);
+            menuItems.push(<ListItem key={2} leftAvatar={<Avatar>MS</Avatar>} primaryText="My Schedule" onClick={() => this.props.history.push('/schedule')}/>);
+            UserService.isCourseProvider().then((isCourseProvider) => {
+                if(isCourseProvider) {
+                    menuItems.push(<ListItem key={3} leftAvatar={<Avatar icon={<FontIcon>add</FontIcon>}/>} primaryText="Add Course" onClick={() => this.props.history.push('/add')}/>);
+                }
+                menuItems.push(<ListItem key={menuItems.length+1} primaryText="Logout" onClick={() => this.logout()}/>)
+            });
+        } else {
+            menuItems.push(<ListItem key={1} primaryText="Login" onClick={() => this.props.history.push('/login')}/>);
+        }
         return (
             <MenuButton
                 id={this.props.id}
                 icon
                 className={this.props.className}
-                menuItems={this.state.user ? [
-                    <ListItem key={1} leftAvatar={<Avatar icon={<FontIcon>account_circle</FontIcon>}/>} primaryText={this.state.user.email}/>,
-                    <ListItem key={2} leftAvatar={<Avatar icon={<FontIcon>add</FontIcon>}/>} primaryText="Add Course" onClick={() => this.props.history.push('/add')}/>,
-                    <ListItem key={3} leftAvatar={<Avatar>MS</Avatar>} primaryText="My Schedule" onClick={() => this.props.history.push('/schedule')}/>,
-                    <ListItem key={4} primaryText="Logout" onClick={() => this.logout()}/>
-                ]: [<ListItem key={1} primaryText="Login" onClick={() => this.props.history.push('/login')}/>]}
-            >
+                menuItems={menuItems}>
                 more_vert
             </MenuButton>
         );
